dbef45cde97186da7effc53cb4ae202416c48f97
404-40-suwapitch <56943@yupparaj.ac.th>
2026-08-05 16:07:03 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -3,7 +3,8 @@ st.title("🛒แอปพลิเคชั่นคำนวณราคาส
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 vat = price * 0.07
 net_price = price - vat
+st.write("นายสุวพิชญ์ กลิ่นจันทร์ เลขที่40 ม.4/4")
+st.divider()
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
-st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
+
